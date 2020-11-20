af8d0aa5a613492d28c6a8d13435f408850c97bf
KyeongMin Park <kio1015@naver.com>
2020-11-21 02:01:33 +0900

    Add preprocess, matching suit function

--- pokerhandTeller/pokerhandTeller/pokerMain.cpp
+++ pokerhandTeller/pokerhandTeller/pokerMain.cpp
@@ -1,5 +1,19 @@
 #include "pokerMain.h"
+#include "shapeDetect.h"
+#include "videocapture.h"
 
 int main() {
+
+	Mat img_src = imread("./card image/img3.png");
+	if (img_src.empty()) {
+		printf("empty");
+		return -1;
+	}
+
+	Card card(img_src);
+	card.preprocess();
+	card.match();
+
+	//videoncapture_basic();
 	return 0;
 }
