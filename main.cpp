c146a45f86f2b5496ca7ff065304c4419f539be3
daust05 <daust05@gmail.com>
2020-11-29 21:53:55 +0900

    distinguish cards

--- main.cpp
+++ main.cpp
@@ -4,16 +4,11 @@
 #include<iostream>
 #include<cstdio>
 #include<math.h>
+#include<algorithm>
 
 using namespace cv;
 using namespace std;
 
-enum point_pos {
-	UP_LEFT,
-	UP_RIGHT,
-	BOTTOM_LEFT,
-	BOTTOM_RIGHT
-};
 //opencv 사용 함수
 //카드 크기 62*88
 /*
@@ -31,42 +26,80 @@ parameter1에는 꼭지점의 개수를 줄일 contour를 넣고
 parameter2로 줄여나갈 오차 epsilon을 넣고 
 parameter3는 폐곡선인지 계곡선인지 여부를 넣는다.
 */
+bool compare_approx_x(Point2f a, Point2f b) {
+	return a.x < b.x;
+}
+bool compare_approx_y(Point2f a, Point2f b) {
+	return a.y < b.y;
+}
 
-void find_edge(Mat* img) {
+void find_cards(Mat* img) {
 	vector<vector<Point> > contours;
 	vector<Point2f> approx;
 
 	Mat img_gray;
 	Mat detected_edges;
-	Mat img_result = (*img).clone();
+	Mat cards[9];
 
 	int lowThreshold = 50;
 	int highThreshold = 150;
 
+
+	/*gray scale image*/
 	cvtColor(*img, img_gray, COLOR_BGR2GRAY);
+	/*detect edges*/
 	blur(img_gray, detected_edges, Size(3, 3));
 	Canny(detected_edges, detected_edges, lowThreshold, highThreshold, 3);
-	findContours(detected_edges, contours, RETR_LIST, CHAIN_APPROX_SIMPLE, Point());
+	/*transform edges into coordinates*/
+	findContours(detected_edges, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point());
 
-	for (size_t i = 0; i < contours.size(); i++){
+	for (int i = 0; i < contours.size(); i++) {
+		/*find vertex using contours*/
 		approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true) * 0.02, true);
+
+		///*sort order of vertex*/
+		int max_index[2];
+		float length, max_width, max = 0;
+		Point2f temp(0, 0);
+		sort(approx.begin(), approx.end(), compare_approx_y);
+		sort(approx.begin(), approx.end(), compare_approx_x);
+		temp = approx[2]; approx[2] = approx[3]; approx[3] = temp;
+
+		/*find longest edge*/
+		for (int i = 0; i < 4; i++) {
+			length = sqrt(pow(approx[i % 4].x - approx[(i + 1) % 4].x, 2) + pow(approx[i % 4].y - approx[(i + 1) % 4].y, 2));
+			if (length > max) {
+				max = length;
+				max_index[0] = i % 4;
+				max_index[1] = (i + 1) % 4;
+			}
+		}
+
+		/* 세로변이 가장 긴 형태일 경우*/
+		if ((max_index[0] == 0 && max_index[1] == 1) ||
+			(max_index[0] == 2 && max_index[1] == 3)) {
+			max_width = max / 88 * 62;
+		}
+		/* 가로변이 가장 긴 형태일 경우*/
+		else {
+			max_width = max / 62 * 88;
+		}
+		
+		vector<Point2f> vertex{
+			Point2f(0, 0), Point2f(0, max),
+			Point2f(max_width, max), Point2f(max_width , 0)
+		};
+		Mat affine = getPerspectiveTransform(approx, vertex);
+		warpPerspective(*img, cards[i], affine, Size(max_width, max));
+		resize(cards[i], cards[i], Size(62, 88), 62 / max_width, 88 / max);
 	}
 
-	float length, max_width, max =0;
-	for (int i = 0; i < 4; i++) {
-		length = sqrt(pow(approx[i%4].x - approx[(i+1)%4].x, 2) + pow(approx[i % 4].y - approx[(i + 1) % 4].y, 2));
-		if (length > max)
-			max = length;
+	/* 테스트 출력*/
+	for (int i = 0; i < contours.size(); i++) {
+		cout << i << " image" << endl;
+  		imshow("Result", cards[i]);
+		waitKey(0);
 	}
-
-	max_width = max / 88 * 62;
-	vector<Point2f> vertex{ 
-		Point2f(0, 0), Point2f(0, max), 
-		Point2f(max_width, max), Point2f(max_width , 0) 
-	};
-	Mat affine = getPerspectiveTransform(approx, vertex);
-	warpPerspective(*img, *img, affine, Size(max_width,max));
-	resize(*img, *img, Size(310, 440), 310 / max_width, 440 / max);
 }
 
 
@@ -76,15 +109,13 @@ int main() {
 	int width = img.cols;
 	int height = img.rows;
 	
-	img = imread("C:/input/card_5.jpg", IMREAD_COLOR);
+	img = imread("C:/input/cards_soft.jpg", IMREAD_COLOR);
 	if (img.empty()) {
 		cout << "이미지를 찾을 수 없음" << endl;
 		return 1;
 	}
 
-	find_edge(&img);
-	imshow("Result", img);
+	find_cards(&img);
 
-	waitKey(0);
 	return 0;
 }
